c1a38e5427f59936f6d0354c09c3ce9cdf3ea759
Luke Bertram <lukebertram@gmail.com>
2018-03-05 17:34:47 -0800

    add transport bpm to state

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,28 +7,30 @@ class Project extends React.Component {
     super(props);
     this.state = {
       isPlaying: false,
+      transportBpm: 100,
       currentMeasure: null,
       playheadPosition: "0:0:0",
       measureArray: [
         {"measurePosition" : 0, "time" : "0:0:0", "pitch" : "C3", "velocity": 0.9},
-        {"measurePosition" : 1, "time" : "0:0:1", "pitch" : null, "velocity": 0.9},
-        {"measurePosition" : 2, "time" : "0:0:2", "pitch" : null, "velocity": 0.9},
-        {"measurePosition" : 3, "time" : "0:0:3", "pitch" : "C4", "velocity": 0.9},
-        {"measurePosition" : 4, "time" : "0:0:4", "pitch" : "B3", "velocity": 0.9},
-        {"measurePosition" : 5, "time" : "0:0:5", "pitch" : null, "velocity": 0.9},
+        {"measurePosition" : 1, "time" : "0:0:1", "pitch" : "D3", "velocity": 0.9},
+        {"measurePosition" : 2, "time" : "0:0:2", "pitch" : "E3", "velocity": 0.9},
+        {"measurePosition" : 3, "time" : "0:0:3", "pitch" : "G3", "velocity": 0.9},
+        {"measurePosition" : 4, "time" : "0:0:4", "pitch" : null, "velocity": 0.9},
+        {"measurePosition" : 5, "time" : "0:0:5", "pitch" : "D4", "velocity": 0.9},
         {"measurePosition" : 6, "time" : "0:0:6", "pitch" : "E4", "velocity": 0.9},
         {"measurePosition" : 7, "time" : "0:0:7", "pitch" : "G4", "velocity": 0.9},
         {"measurePosition" : 8, "time" : "0:0:8", "pitch" : null, "velocity": 0.9},
-        {"measurePosition" : 9, "time" : "0:0:9", "pitch" : "C4", "velocity": 0.9},
-        {"measurePosition" : 10, "time" : "0:0:10", "pitch" : "B3", "velocity": 0.9},
-        {"measurePosition" : 11, "time" : "0:0:11", "pitch" : null, "velocity": 0.9},
-        {"measurePosition" : 12, "time" : "0:0:12", "pitch" : null, "velocity": 0.9},
-        {"measurePosition" : 13, "time" : "0:0:13", "pitch" : null, "velocity": 0.9},
-        {"measurePosition" : 14, "time" : "0:0:14", "pitch" : null, "velocity": 0.9},
-        {"measurePosition" : 15, "time" : "0:0:15", "pitch" : null, "velocity": 0.9}
+        {"measurePosition" : 9, "time" : "0:0:9", "pitch" : "D5", "velocity": 0.9},
+        {"measurePosition" : 10, "time" : "0:0:10", "pitch" : "E5", "velocity": 0.9},
+        {"measurePosition" : 11, "time" : "0:0:11", "pitch" : "G5", "velocity": 0.9},
+        {"measurePosition" : 12, "time" : "0:0:12", "pitch" : "C6", "velocity": 0.9},
+        {"measurePosition" : 13, "time" : "0:0:13", "pitch" : "D6", "velocity": 0.9},
+        {"measurePosition" : 14, "time" : "0:0:14", "pitch" : "E6", "velocity": 0.9},
+        {"measurePosition" : 15, "time" : "0:0:15", "pitch" : "G6", "velocity": 0.9}
       ],
     };
     this.transport = Tone.Transport;
+    this.transport.bpm.value = this.state.transportBpm;
     this.transport.setLoopPoints("0:0:0", "1:0:0");
     this.transport.loop = true;
 
@@ -37,7 +39,7 @@ class Project extends React.Component {
     	//the value is an object which contains both the note and the velocity
       const synth = new Tone.Synth();
       synth.toMaster();
-    	synth.triggerAttackRelease(value.pitch, "8n", time, value.velocity);
+    	synth.triggerAttackRelease(value.pitch, "16n", time, value.velocity);
     }, this.state.measureArray.filter((note) => note.pitch)
     ).start("0:0:0");
     console.log(this.state.measureArray);
@@ -48,7 +50,7 @@ class Project extends React.Component {
 
   handleStartMeasurePlayback(){
     console.log(`Start playback pressed`);
-    this.transport.start("+0.05", "0:0:0");
+    this.transport.start("+0.1", "0:0:0");
     this.setState({isPlaying: true});
     setTimeout(()=>{console.log(`State of isPlaying: ${this.state.isPlaying}`)}, 100);
   };
