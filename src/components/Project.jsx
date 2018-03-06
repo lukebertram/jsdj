595e554af8d23561cfab36a25941df61c8b541e4
Luke Bertram <lukebertram@gmail.com>
2018-03-06 09:09:26 -0800

    add logic to display and update playhead location in measure view

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -15,18 +15,18 @@ class Project extends React.Component {
         {"measurePosition" : 1, "time" : "0:0:1", "pitch" : "D3", "velocity": 0.9},
         {"measurePosition" : 2, "time" : "0:0:2", "pitch" : "E3", "velocity": 0.9},
         {"measurePosition" : 3, "time" : "0:0:3", "pitch" : "G3", "velocity": 0.9},
-        {"measurePosition" : 4, "time" : "0:0:4", "pitch" : null, "velocity": 0.9},
-        {"measurePosition" : 5, "time" : "0:0:5", "pitch" : "D4", "velocity": 0.9},
-        {"measurePosition" : 6, "time" : "0:0:6", "pitch" : "E4", "velocity": 0.9},
-        {"measurePosition" : 7, "time" : "0:0:7", "pitch" : "G4", "velocity": 0.9},
-        {"measurePosition" : 8, "time" : "0:0:8", "pitch" : null, "velocity": 0.9},
-        {"measurePosition" : 9, "time" : "0:0:9", "pitch" : "D5", "velocity": 0.9},
-        {"measurePosition" : 10, "time" : "0:0:10", "pitch" : "E5", "velocity": 0.9},
-        {"measurePosition" : 11, "time" : "0:0:11", "pitch" : "G5", "velocity": 0.9},
-        {"measurePosition" : 12, "time" : "0:0:12", "pitch" : "C6", "velocity": 0.9},
-        {"measurePosition" : 13, "time" : "0:0:13", "pitch" : "D6", "velocity": 0.9},
-        {"measurePosition" : 14, "time" : "0:0:14", "pitch" : "E6", "velocity": 0.9},
-        {"measurePosition" : 15, "time" : "0:0:15", "pitch" : "G6", "velocity": 0.9}
+        {"measurePosition" : 4, "time" : "0:1:0", "pitch" : "C4", "velocity": 0.9},
+        {"measurePosition" : 5, "time" : "0:1:1", "pitch" : "D4", "velocity": 0.9},
+        {"measurePosition" : 6, "time" : "0:1:2", "pitch" : "E4", "velocity": 0.9},
+        {"measurePosition" : 7, "time" : "0:1:3", "pitch" : "G4", "velocity": 0.9},
+        {"measurePosition" : 8, "time" : "0:2:0", "pitch" : "C5", "velocity": 0.9},
+        {"measurePosition" : 9, "time" : "0:2:1", "pitch" : "D5", "velocity": 0.9},
+        {"measurePosition" : 10, "time" : "0:2:2", "pitch" : "E5", "velocity": 0.9},
+        {"measurePosition" : 11, "time" : "0:2:3", "pitch" : "G5", "velocity": 0.9},
+        {"measurePosition" : 12, "time" : "0:3:0", "pitch" : "C6", "velocity": 0.9},
+        {"measurePosition" : 13, "time" : "0:3:1", "pitch" : "D6", "velocity": 0.9},
+        {"measurePosition" : 14, "time" : "0:3:2", "pitch" : "E6", "velocity": 0.9},
+        {"measurePosition" : 15, "time" : "0:3:3", "pitch" : "G6", "velocity": 0.9}
       ],
     };
     this.transport = Tone.Transport;
@@ -48,11 +48,18 @@ class Project extends React.Component {
     this.handleStopMeasurePlayback = this.handleStopMeasurePlayback.bind(this);
   }
 
+  // componentDidMount(){
+  //   setInterval(function(){ this.setState({playheadPosition: this.transport.position});}, 500);
+  // }
+
   handleStartMeasurePlayback(){
     console.log(`Start playback pressed`);
+    this.transport.scheduleRepeat(()=>{ this.setState({ playheadPosition: this.transport.position })}, "16n");
     this.transport.start("+0.1", "0:0:0");
     this.setState({isPlaying: true});
     setTimeout(()=>{console.log(`State of isPlaying: ${this.state.isPlaying}`)}, 100);
+    // setInterval( () => { this.setState({ playheadPosition: this.transport.position })}, 100);
+    setInterval( () => { console.log(`Playhead Position in state: ${this.state.playheadPosition}`)}, 100);
   };
 
   handleStopMeasurePlayback(){
